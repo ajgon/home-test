d09ded443769039e437d793552eac5c685d51b7e
Igor Rzegocki <igor@rzegocki.pl>
2025-04-02 22:22:51 +0200

    WIP

--- scripts/bootstrap-cluster.sh
+++ scripts/bootstrap-cluster.sh
@@ -228,7 +228,7 @@ function main() {
     # wait_for_nodes
     # wipe_rook_disks
     # apply_crds
-    # apply_resources
+    apply_resources
     apply_helm_releases
 
     log info "Congrats! The cluster is bootstrapped and ArgoCD is syncing the Git repository"
